7139fb5157707c1a4dd7d443f3e911e193b22996
solaryan <466010106@qq.com>
2024-06-06 17:21:11 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use axum::{routing::get, Router};
 use log;
+use reqwest::Method;
 use rusite::{api::api_route, error::Error, utils::AppState};
+use tower_http::cors::{any, CorsLayer};
 
 async fn initialize() -> Result<AppState, Error> {
     dotenv::dotenv().ok();
@@ -14,7 +16,10 @@ async fn main () -> Result<(), Error> {
 
     let app = Router::new()
         .route("/", get(|| async {"running..."}))
-        .nest("/api", api_route(state));
+        .nest("/api", api_route(state))
+        .layer(CorsLayer::new()
+                    .allow_methods(vec![Method::GET, Method::POST])
+                    .allow_origin(any()));
 
         let listener = tokio::net::TcpListener::bind("0.0.0.0:8216").await.unwrap();
         log::info!("LISTENING on {:?}\n", listener.local_addr());
